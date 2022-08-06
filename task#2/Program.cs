e228c58d03b9180ad50309061c887983d7dcffe3
Alexey <alexey_beerman@mail.ru>
2022-08-06 15:04:19 +0300

    add new content task#2

--- task#2/Program.cs
+++ task#2/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
 
 Console.WriteLine("Введите первое число ");
 int numberFirst = Convert.ToInt32(Console.ReadLine());
@@ -6,13 +7,20 @@ int numberFirst = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите следующее число ");
 int numberSecond = Convert.ToInt32(Console.ReadLine());
 
-if (numberFirst > numberSecond)
+if (numberFirst == numberSecond)
 {
-    Console.Write($"max = {numberFirst} ");
-    Console.Write($"min = {numberSecond} ");
+    Console.Write($"Числа равны друг другу! Первое число - {numberFirst} , Второе число - {numberSecond}");
 }
 else
 {
-    Console.Write($"max = {numberSecond} ");
-    Console.Write($"min = {numberFirst} ");
+    if (numberFirst > numberSecond)
+    {
+        Console.WriteLine($"Максимальное число = {numberFirst} ");
+        Console.Write($"Минимальное число = {numberSecond} ");
+    }
+    else
+    {
+        Console.WriteLine($"Максимальное число = {numberSecond} ");
+        Console.Write($"Минимальное число = {numberFirst} ");
+    }
 }
