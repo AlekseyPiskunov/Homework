20d6cb3522594aafce674243e03213ba09101a13
Alexey <alexey_beerman@mail.ru>
2022-08-11 23:47:10 +0300

    add info task#15

--- HomeWork(2)/task#15/Program.cs
+++ HomeWork(2)/task#15/Program.cs
@@ -1,53 +1,106 @@
 ﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-Console.Write("Введите число от 1 до 7 , соответствующее дню недели ");
+
+// Console.Write("Введите число от 1 до 7 , соответствующее дню недели ");
+// int numberDay = Convert.ToInt32(Console.ReadLine());
+
+// switch (numberDay)
+// {
+//     case 1:
+//         Console.Write("Понедельник");
+//         break;
+
+//     case 2:
+//         Console.Write("Вторник");
+//         break;
+
+//     case 3:
+//         Console.Write("Среда");
+//         break;
+
+//     case 4:
+//         Console.Write("Четверг");
+//         break;
+
+//     case 5:
+//         Console.Write("Пятница");
+//         break;
+
+//     case 6:
+//         Console.Write("Суббота");
+//         break;
+
+//     case 7:
+//         Console.Write("Воскресенье");
+//         break;
+// }
+
+// void DayWeek(int day)
+// {
+//     if (day == 6 || day == 7)
+//     {
+//         Console.WriteLine(" - этот день является выходным.");
+//         return;
+//     }
+//     if (day > 7 || day < 1)
+//     {
+//         Console.WriteLine($"{day} - это не день недели.");
+//     }
+//     else
+//     {
+//         Console.WriteLine(" - это будний день.");
+//     }
+// }
+
+// DayWeek(numberDay);
+
+Console.WriteLine("Введите число соответствующее дню недели ");
 int numberDay = Convert.ToInt32(Console.ReadLine());
 
 switch (numberDay)
 {
     case 1:
-        Console.Write("Понедельник");
+        Console.WriteLine("Понедельник.");
         break;
-
     case 2:
-        Console.Write("Вторник");
+        Console.WriteLine("Вторник.");
         break;
-
     case 3:
-        Console.Write("Среда");
+        Console.WriteLine("Среда.");
         break;
-
     case 4:
-        Console.Write("Четверг");
+        Console.WriteLine("Четверг.");
         break;
-
     case 5:
-        Console.Write("Пятница");
+        Console.WriteLine("Пятница.");
         break;
-
     case 6:
-        Console.Write("Суббота");
+        Console.WriteLine("Суббота.");
         break;
-
     case 7:
-        Console.Write("Воскресенье");
+        Console.WriteLine("Воскресенье.");
         break;
 }
 
-void DayWeek(int day)
+void LookingDay(int num)
 {
-    if (day == 6 || day == 7)
+    if ((num < 1) || (num > 7))
     {
-        Console.WriteLine(" - этот день является выходным.");
-        return;
-    }
-    if (day > 7 || day < 1)
-    {
-        Console.WriteLine($"{day} - это не день недели.");
-    }
-    else
-    {
-        Console.WriteLine(" - это будний день.");
+        throw new Exception("Не существует");
     }
 }
 
-DayWeek(numberDay);
+bool LookingDayOff(int num)
+{
+    LookingDay(numberDay);
+    bool result = num == 6 || num == 7;
+    return result;
+}
+
+if (LookingDayOff(numberDay))
+{
+    Console.WriteLine("Является выходным днём!");
+}
+else
+{
+    Console.WriteLine("Не является выходным днём!");
+}
