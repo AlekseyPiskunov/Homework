45dd9ec93ffb0f1cd430f679fc99a1d6301a32a5
Alexey <alexey_beerman@mail.ru>
2022-08-13 21:17:48 +0300

    add new content task#23

--- HomeWork(3)/task#23/Program.cs
+++ HomeWork(3)/task#23/Program.cs
@@ -1,17 +1,42 @@
-﻿/*
-    Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 
+/* 
+     Вариант 1 
 */
-try
-{
-    System.Console.WriteLine("Введите число ");
-    int number = int.Parse(Console.ReadLine());
+//try
+// {
+//     System.Console.WriteLine("Введите число ");
+//     int number = int.Parse(Console.ReadLine());
 
-    for (int i = 1; i <= number; i++)
-    {
-        System.Console.WriteLine(Math.Pow((i), 3));
-    }
-}
-catch (Exception)
+//     for (int i = 1; i <= number; i++)
+//     {
+//         System.Console.WriteLine(Math.Pow((i), 3));
+//     }
+// }
+// catch (Exception)
+// {
+//     System.Console.WriteLine("Error.");
+// }
+
+/*  
+    Вариант 2 
+*/
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] TableСube(int s)
 {
-    System.Console.WriteLine("Error.");
+    double[] array = new double[s];
+    for (int i = 1; i <= n; i++)
+    {
+        array[i - 1] = Math.Pow((i), 3);
+    }
+    return array;
+}
+
+double[] result = TableСube(n);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
 }
