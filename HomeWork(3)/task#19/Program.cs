36b71638ea9088c43bde24256a4b9eadff9dcf88
Alexey <alexey_beerman@mail.ru>
2022-08-16 01:00:20 +0300

    new content task#19

--- HomeWork(3)/task#19/Program.cs
+++ HomeWork(3)/task#19/Program.cs
@@ -1,97 +1,28 @@
 ﻿//______________________________________________________________
 // Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-/* Вариант 1 */
-// try
-// {
-//     Console.WriteLine("Введите пятизначное число: ");
-//     int number = int.Parse(Console.ReadLine());
-//     string numberStr = Convert.ToString(number);
+System.Console.Write("Введите число -  ");
+string? firstNumber = Console.ReadLine();
+string secondNumber = string.Empty;
 
-//     if (numberStr.Length == 5)
-//     {
-//         Checking(numberStr);
-//     }
-//     else
-//     {
-//         Console.WriteLine("Я не знаю что с этим делать...");
-//     }
-// }
-// catch (Exception)
-// {
-//     Console.WriteLine("Ошибка ввода!");
-// }
-
-// void Checking(string num)
-// {
-//     if (num[0] == num[4] & num[1] == num[3])
-//     {
-//         Console.WriteLine("Это число палиндром.");
-//     }
-//     else
-//     {
-//         Console.WriteLine("Это число не палиндром.");
-//     }
-// }
-
-/* 
-    Вариант 2 
-*/
-
-// try
-// {
-//     Console.WriteLine("Введите пятизначное число: ");
-//     int number = int.Parse(Console.ReadLine());
-//     string numberStr = Convert.ToString(number);
-
-//     if (numberStr.Length == 5)
-//     {
-//         if (Checking(numberStr))
-//         {
-//             Console.WriteLine("Палиндром!");
-//         }
-//         else
-//         {
-//             Console.WriteLine("Не палиндром!");
-//         }
-//     }
-//     else
-//     {
-//         System.Console.WriteLine("Error.");
-//     }
-// }
-// catch
-// {
-//     throw new Exception("Error.");
-// }
-
-// bool Checking(string num)
-// {
-//     bool result = num[0] == num[4] || num[1] == num[3];
-//     return result;
-// }
-
-/*
-    Вариант 3
-*/
-Console.WriteLine("Введите пятизначное число: ");
-
-int Reverse(int number)
+for (int i = firstNumber.Length - 1; i >= 0; i--)
 {
-    string numberStr = Convert.ToString(number);
-    char[] c = numberStr.ToCharArray();
-    Array.Reverse(c);
-    string n = new string(c);
-    return Convert.ToInt32(n);
+    secondNumber += firstNumber[i];
 }
 
-int number = Convert.ToInt32(Console.ReadLine());
-int Rev = Reverse(number);
-if (Rev == number)
+for (int i = 0; i < secondNumber.Length; i++)
 {
-    Console.WriteLine("Палиндром ");
+    System.Console.Write($" {secondNumber[i]}");
 }
-else
+
+bool Checks(string num1, string num2)
 {
-    Console.WriteLine("Не палиндром ");
+    bool checks = num1 == num2;
+    return checks;
 }
+
+if (Checks(firstNumber, secondNumber))
+{
+    System.Console.WriteLine(" | Это палиндром.");
+}
+else System.Console.WriteLine(" | Это не палиндром.");
--- HomeWork(3)/task#23/Program.cs
+++ HomeWork(3)/task#23/Program.cs
@@ -1,26 +1,5 @@
 ﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 
-/* 
-     Вариант 1 
-*/
-//try
-// {
-//     System.Console.WriteLine("Введите число ");
-//     int number = int.Parse(Console.ReadLine());
 
-//     for (int i = 1; i <= number; i++)
-//     {
-//         System.Console.WriteLine(Math.Pow((i), 3));
-//     }
-// }
-// catch (Exception)
-// {
-//     System.Console.WriteLine("Error.");
-// }
-
-/*  
-    Вариант 2 
-*/
 Console.WriteLine("Введите число ");
 int n = Convert.ToInt32(Console.ReadLine());
 
