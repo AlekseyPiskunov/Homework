e7858b297331f2325e6444b84d1c191d13b5ec21
Alexey <alexey_beerman@mail.ru>
2022-08-20 16:53:14 +0300

    add new content HomeWork(1)/task#8

--- HomeWork(1)/task#8/Program.cs
+++ HomeWork(1)/task#8/Program.cs
@@ -1,4 +1,5 @@
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Вариант 1.
 Console.Clear();
 
 Console.WriteLine("Напишите любое число ");
@@ -9,3 +10,62 @@ for (int i = 2; i <= n; i = i + 2)
     Console.Write(i);
     Console.Write("| ");
 }
+//______________________________________________________
+//Вариант 2.
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int[] FillNewArray(int num)
+// {
+//     int j = 0;
+//     int[] array = new int[num / 2];
+//     for (int i = 1; i <= num; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             array[j] = i;
+//             j++;
+//         }
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         System.Console.Write($"{arr[i]} ");
+//     }
+// }
+
+// int[] result = FillNewArray(number);
+// PrintArray(result);
+
+//______________________________________________________
+// Вариант 3.
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// List<int> FillNewArray(int num)
+// {
+//     List<int> list = new List<int>();
+//     for (int i = 1; i <= num; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             list.Add(i);
+//         }
+//     }
+//     return list;
+// }
+
+// void PrintArray(List<int> list)
+// {
+//     for (int i = 0; i < list.Count; i++)
+//     {
+//         System.Console.Write($"{list[i]} ");
+//     }
+// }
+
+// List<int> list = FillNewArray(number);
+// PrintArray(list);
